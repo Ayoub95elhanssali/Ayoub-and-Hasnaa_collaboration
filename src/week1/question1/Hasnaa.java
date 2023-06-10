58746e8f030b29c7b24478c87c827dafff329082
hasnaa <hasnaalamtahri@gmail.com>
2023-06-10 23:15:27 +0000

    Even&Odd

--- src/week1/question1/Hasnaa.java
+++ src/week1/question1/Hasnaa.java
@@ -1,6 +1,13 @@
 package week1.question1;
 
 public class Hasnaa {
+    public static String identify(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
 
 
 
