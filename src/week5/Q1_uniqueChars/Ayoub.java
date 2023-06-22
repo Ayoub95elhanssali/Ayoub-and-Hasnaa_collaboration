cd85a9f36ae58851cf168c6ebac7339117e462a3
ayoub <Ayoub.hanssali95@gmail.com>
2023-06-22 10:46:18 -0500

    update

---
+++ src/week5/Q1_uniqueChars/Ayoub.java
@@ -0,0 +1,8 @@
+package week5.Q1_uniqueChars;
+
+public class Ayoub {
+}
+/*
+    Write a return  method that can find the unique characters from the String
+    Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+     */
